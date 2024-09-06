d4f323a1dc205d0a1820ee38e51d6565ee782918
Vadim Karcev <cfg888powerfull@gmail.com>
2024-09-06 14:59:27 +0300

    Shadowed default __name__ and __anotations__ of Handler class, reworked name getter for types (fixed for None-type objects), added string representation for handler

--- telebot/handler.py
+++ telebot/handler.py
@@ -1,10 +1,10 @@
-from typing import Callable, Any
+from typing import Callable, Any, Type
 
 class Handler:
     def __init__(self, handler: Callable):
         self.__handler: Callable = handler
-        self.__name: str = handler.__name__
-        self.__annotations: dict[str, Any] = handler.__annotations__
+        if len(self.__annotations__) == 0:
+            raise ValueError(f'Function {self.__name__}() must have type annotation')
 
 
     def __call__(self, *args, **kwargs):
@@ -16,7 +16,7 @@ class Handler:
 
     def __checkTypesCorrespondence(self, *args: list[Any], **kwargs: dict[str, Any]) -> tuple[bool, str | None]:
 
-        handler_args = list(self.__annotations.keys())
+        handler_args = list(self.__annotations__.keys())
         if 'return' in handler_args:
             handler_args.remove('return')
         handler_args_len = len(handler_args)
@@ -24,25 +24,25 @@ class Handler:
 
         # Checking args count
         if given_args_len > handler_args_len:
-            return False, f'Function {self.__name}() takes {handler_args_len} arguments but {given_args_len} were given'
+            return False, f'Function {self.__name__}() takes {handler_args_len} arguments but {given_args_len} were given'
 
         # Checking positional arguments
         for narg, arg in enumerate(args):
             arg_name = handler_args.pop(0)
-            if not isinstance(arg, self.__annotations[arg_name]):
-                return False, f'Function {self.__name}() takes {self.__annotations[arg_name].__name__} as argument {arg_name} (position {narg + 1}) but {type(arg).__name__} was given'
+            if not isinstance(arg, self.__annotations__[arg_name]):
+                return False, f'Function {self.__name__}() takes {self.__className(self.__annotations__[arg_name])} as argument {arg_name} (position {narg + 1}) but {self.__className(type(arg))} was given'
         
         # Checking named arguments (also checking if this argument was already 
         # passed throw positional arguments)
         for arg_name, arg in list(kwargs.items()):
-            if arg_name not in self.__annotations.keys():
-                return False, f'Function {self.__name}() has no argument {arg_name}'
+            if arg_name not in self.__annotations__.keys():
+                return False, f'Function {self.__name__}() has no argument {arg_name}'
 
             elif arg_name not in handler_args:
-                return False, f'Function {self.__name}() was already gotten {arg_name} as positional argument'
+                return False, f'Function {self.__name__}() was already gotten {arg_name} as positional argument'
 
-            elif not isinstance(arg, self.__annotations[arg_name]):
-                return False, f'Function {self.__name}() takes {self.__annotations[arg_name].__name__} as argument {arg_name} but {type(arg).__name__} was given'
+            elif not isinstance(arg, self.__annotations__[arg_name]):
+                return False, f'Function {self.__name__}() takes {self.__className(self.__annotations__[arg_name])} as argument {arg_name} but {self.__className(type(arg))} was given'
 
         return True, None
     
@@ -50,3 +50,34 @@ class Handler:
     def correspondes(self, *args: list[Any], **kwars: dict[str, Any]) -> bool:
         correct, _ = self.__checkTypesCorrespondence(args, kwars)
         return correct
+    
+    @property
+    def __annotations__(self) -> dict[str, Any]:
+        return self.__handler.__annotations__
+    
+    @property
+    def __name__(self) -> str:
+        return self.__handler.__name__
+    
+    def __str__(self) -> str:
+
+        args: list[str] = []
+        ret: str | None = None
+
+        for arg, type in self.__annotations__.items():
+            if arg != 'return':
+                args.append(f'{arg}: {self.__className(type)}')
+            else:
+                ret = self.__className(type)
+
+        args_str = ', '.join(args)
+        ret_str = '' if ret is None else f' -> {ret}'
+        
+        return f'<function {self.__name__}({args_str}){ret_str}>'
+    
+    @staticmethod
+    def __className(cls: Type | None):
+        if cls is None:
+            return 'None'
+        else:
+            return cls.__name__
