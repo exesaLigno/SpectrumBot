23a5edc9419c6ab2b9985a5ea8ecf13ac4cb8d25
Vadim Karcev <cfg888powerfull@gmail.com>
2024-11-15 14:40:05 +0300

    Added get_me support, added support of inline keyboards

--- telebot/api.py
+++ telebot/api.py
@@ -30,9 +30,13 @@ class TelegramAPI(object):
 
         return responce
     
+    def getMe(self):
+        responce = self.__makeRequest("getMe")
+        if responce['connection_established'] and responce['ok']:
+            return responce['result']
+    
     def __downloadFile(self, file_path: str):
         request_string = f'https://api.telegram.org/file/bot{self.token}/{file_path}'
-        print(request_string)
 
         responce = {"connection_established": False, "ok": False}
 
@@ -58,20 +62,40 @@ class TelegramAPI(object):
             return responce['file']
         return None
     
-    def sendMessage(self, chat_id, text: str | None, photo: BytesIO | bytes | None = None, reply_to: int | None = None):
+    def sendMessage(
+            self, chat_id, 
+            text: str | None, 
+            photo: BytesIO | bytes | None = None, 
+            reply_to: int | None = None, 
+            keyboard: list[list[tuple[str, str]]] | None = None
+        ):
+        
         reply_params = None
         if reply_to is not None:
             reply_params = dumps({'message_id': reply_to})
+
+        reply_markup = None
+        if keyboard is not None:
+            for line_no in range(len(keyboard)):
+                for key_no in range(len(keyboard[line_no])):
+                    keyboard[line_no][key_no] = {
+                        'text': keyboard[line_no][key_no][0], 
+                        'callback_data': keyboard[line_no][key_no][1]
+                    }
+            reply_markup = dumps({"inline_keyboard": keyboard}, ensure_ascii = False)
 
         if photo is not None:
             if isinstance(photo, BytesIO):
                 photo = photo.read()
             
-            return self.__makeRequest('sendPhoto', post_arguments={'photo': photo}, chat_id=chat_id, caption=text, reply_parameters=reply_params)
+            return self.__makeRequest('sendPhoto', post_arguments={'photo': photo}, chat_id=chat_id, caption=text, reply_parameters=reply_params, reply_markup = reply_markup, parse_mode = "HTML")
         
         else:
-            return self.__makeRequest('sendMessage', chat_id=chat_id, text=text, reply_parameters=reply_params)
+            return self.__makeRequest('sendMessage', chat_id=chat_id, text=text, reply_parameters=reply_params, reply_markup = reply_markup, parse_mode = "HTML")
         
+    def answerCallbackQuery(self, query_id: int, text: str, show_alert: bool = False):
+        return self.__makeRequest("answerCallbackQuery", callback_query_id = query_id, text = text, show_alert = show_alert)
+    
     def deleteMessage(self, chat_id, message_id):
         return self.__makeRequest('deleteMessage', chat_id=chat_id, message_id=message_id)
     
