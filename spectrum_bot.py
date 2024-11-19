a4cfd3e887a64d38f23c128265c0b6752abff472
Vadim Karcev <cfg888powerfull@gmail.com>
2024-11-19 12:46:01 +0300

    Added template of re-processing audio with different settings, added telemetry collection

--- spectrum_bot.py
+++ spectrum_bot.py
@@ -4,8 +4,13 @@ from telebot.database import DBSettings
 from telebot.events import Message, Callback
 from spectrum import AudioSpectre, Scale, Limits
 from time import time
+from telebot.dumpable_dictionary import DDict
 
 DBSettings.path = 'test.db'
+
+telemetry = DDict('telemetry.json')
+
+#cached_spectres = {}
 
 with open('token.secret', 'r') as token_file:
     token_str = token_file.read().strip(' \n')
@@ -14,22 +19,77 @@ bot = TelegramBot(token=token_str)
 
 @bot.registerHandler(type=Message, message_type=Message.Type.VOICE_MESSAGE)
 def handleVoiceMessage(message: Message) -> None:
+
+    if 'processings_count' in telemetry:
+        telemetry['processings_count'] += 1
+    else:
+        telemetry['processings_count'] = 1
+
+    if 'unique_users' in telemetry:
+        if message.chat_id not in telemetry['unique_users']:
+            telemetry['unique_users'].append(message.chat_id)
+    else:
+        telemetry['unique_users'] = [message.chat_id]
+        
+
     notification = message.reply(text='Processing your audio')
 
     audio_file = message.getFile(message.media['file_id'])
     s = AudioSpectre(audio_file)
     photo = s.Spectre(scale=Scale.LINEAR, limits=Limits.FULL)
 
-    message.reply(text='Here is the spectre of provided audio', photo=photo, 
-                  keyboard=[
-                      [('Switch to logarithmic scale', 'logscale')], 
-                      [('Fit to content', 'fit-content')]
-                  ])
-    notification.delete()
+    spectre = notification.edit(text='Here is the spectre of provided audio', photo=photo, 
+                                keyboard=[
+                                    [('Switch to logarithmic scale', 'logscale')], 
+                                    [('Fit to content', 'fit-content')]
+                                ])
+    
+    #cached_spectres[spectre.message_id] = {'spectre': s, 'scale': Scale.LINEAR, 'limits': Limits.FULL}
+
 
 @bot.registerHandler(type=Callback)
 def handleCallback(callback: Callback):
-    callback.answer('Not implemented', show_alert=False)
+
+    if callback.data in ['logscale', 'linscale', 'fit-content', 'full']:
+
+        callback.answer('Switching to different scales and limits are not implemented!', show_alert=True)
+        
+        # s: AudioSpectre = cached_spectres[callback.message.message_id]['spectre']
+
+        # if callback.data == 'logscale':
+        #     cached_spectres[callback.message.message_id]['scale'] = Scale.LOGARITHMIC
+            
+        # elif callback.data == 'linscale':
+        #     cached_spectres[callback.message.message_id]['scale'] = Scale.LINEAR
+
+        # elif callback.data == 'fit-content':
+        #     cached_spectres[callback.message.message_id]['limits'] = Limits.FIT_TO_DATA
+
+        # elif callback.data == 'full':
+        #     cached_spectres[callback.message.message_id]['limits'] = Limits.FULL
+        
+        # else:
+        #     pass
+
+        # photo = s.Spectre(scale=cached_spectres[callback.message.message_id]['scale'], 
+        #                   limits=cached_spectres[callback.message.message_id]['limits'])
+        
+        # if cached_spectres[callback.message.message_id]['scale'] == Scale.LINEAR:
+        #     scale_but = ('Switch to logarithmic scale', 'logscale')
+        # else:
+        #     scale_but = ('Switch to linear scale', 'linscale')
+
+        # if cached_spectres[callback.message.message_id]['limits'] == Limits.FULL:
+        #     limits_but = ('Fit to content', 'fit-content')
+        # else:
+        #     limits_but = ('Show full spectre', 'full')
+
+        # keyboard = [[scale_but], [limits_but]]
+
+        # callback.message.edit(text='Here is the spectre of provided audio', photo=photo, keyboard=keyboard)
+
+    else:
+        callback.answer('Internal error!', show_alert=True)
 
 @bot.registerHandler(type=Message, message_type=Message.Type.COMMAND)
 def handleCommand(message: Message) -> None:
@@ -50,4 +110,4 @@ def handleCommand(message: Message) -> None:
 # def PreHandleVoiceMessage(message: Message) -> None:
 #     print(f'Proc')
 
-bot.polling()
+bot.polling(supress_exceptions=True)
